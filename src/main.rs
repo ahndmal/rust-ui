15e9a03467bd9371159bf9630732990b7a3b917d
Andrii Maliuta <quadr988@gmail.com>
2022-11-29 21:04:21 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ const APP_ID: &str = "Rust UI App";
 
 fn on_activate(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
-    let close_button = gtk::Button::with_label("Close");
+    let close_button = gtk::Button::builder()
+        .label("Close").height_request(30).width_request(100).name("Close").build();
+    close_button.height_request();
+    close_button.width_request();
     let main_stack = gtk::Stack::builder().name("Stack 1").height_request(600).build();
+    let box1 = gtk::Box::builder().name("box 1").tooltip_text("tooltip").build();
     close_button.connect_clicked(clone!(@weak window => move |_| window.close()));
+
     window.set_child(Some(&main_stack));
+    window.set_child(Some(&box1));
     window.set_child(Some(&close_button));
     window.present();
 }
